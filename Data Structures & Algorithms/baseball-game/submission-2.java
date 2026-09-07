7574ab71520aad95115e42112b62c1b7fbe8ddd1
Aadarsh Katigi <76147025+Adarxhhhh@users.noreply.github.com>
2026-09-06 23:29:36 -0500

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(String str : operations){
+            switch(str){
+                case "+" : int a = stack.pop();
+                            int b = stack.peek();
+                            int sum = a + b;
+                            stack.push(a);
+                            stack.push(sum);
+                            break;
+                case "C" : stack.pop();
+                            break;
+                case "D" : stack.push(2 * stack.peek());
+                            break;
+                default : stack.push(Integer.parseInt(str));
+            }
+        }
+
+        int sum = 0;
+        while(!stack.isEmpty()){
+            sum += stack.pop();
+        }
+
+    return sum;
+    }
+}

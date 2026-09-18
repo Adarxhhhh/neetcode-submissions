f66dc0a54e4c0fbfedcc7d3b232fb1e8a5043ae8
Aadarsh Katigi <76147025+Adarxhhhh@users.noreply.github.com>
2026-09-18 09:18:41 -0500

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        Queue<int []> queue = new LinkedList<>();
+
+        for(int i = 0;i < m; i++){
+            if(board[i][0] == 'O') enqueue(board, i, 0, queue);
+            if(board[i][n - 1] == 'O') enqueue(board,i, n - 1, queue);
+        }
+
+        for(int j = 0; j < n; j++){
+            if(board[0][j] == 'O') enqueue(board, 0, j, queue);
+            if(board[m - 1][j] == 'O') enqueue(board, m - 1, j, queue);
+        }
+
+        int [][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        while(!queue.isEmpty()){
+            int [] curr = queue.poll();
+
+            for(int [] dir : dirs){
+                int nr = dir[0] + curr[0];
+                int nc = dir[1] + curr[1];
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && board[nr][nc] == 'O'){
+                    enqueue(board, nr, nc, queue);
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }else if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void enqueue(char [][] board, int i, int j, Queue<int []> queue){
+        board[i][j] = '#';
+        queue.offer(new int [] {i, j});
+    }
+}

5481f05782dafb02d2db3edf0c0f281d0f204299
Aadarsh Katigi <76147025+Adarxhhhh@users.noreply.github.com>
2026-09-19 10:40:21 -0500

    Add: open-the-lock - submission-0

---
+++ Data Structures & Algorithms/open-the-lock/submission-0.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> deadlock = new HashSet<>(Arrays.asList(deadends));
+        String start = "0000";
+        if(deadlock.contains(start)) return -1;
+        if(target.equals(start)) return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start);
+        Set<String> visited = new HashSet<>();
+        visited.add(start);
+        int turns = 0;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            turns++;
+
+            for(int i = 0; i < size; i++){
+                String curr = queue.poll();
+
+                for(String next : neighbors(curr)){
+                    if(deadlock.contains(next) || visited.contains(next)) continue;
+                    if(next.equals(target)) return turns;
+
+                    visited.add(next);
+                    queue.offer(next);
+                }
+            }
+        }
+        return -1;
+    }
+
+    public List<String> neighbors(String state){
+        List<String> res = new ArrayList<>(8);
+        char [] chars = state.toCharArray();
+
+        for(int i = 0; i < 4; i++){
+            char curr = chars[i];
+
+            chars[i] = curr == '9' ? '0' : (char)(curr + 1);
+            res.add(new String(chars));
+            chars[i] = curr == '0' ? '9' : (char)(curr - 1);
+            res.add(new String(chars));
+            chars[i] = curr;
+        }
+
+        return res;
+    }
+}

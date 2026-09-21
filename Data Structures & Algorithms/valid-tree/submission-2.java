e8c84ef1b2701d6a5ec458852f85073ac6a97402
Aadarsh Katigi <76147025+Adarxhhhh@users.noreply.github.com>
2026-09-21 14:02:25 -0500

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if(edges.length != n - 1) return false;
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int [] edge : edges){
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean [] visited = new boolean[n];
+
+        if(hasCycle(0, -1, graph, visited)) return false; 
+
+        for(boolean v : visited){
+            if(!v) return false;
+        }
+
+        return true;
+    }
+
+    public boolean hasCycle(int node, int parent, List<List<Integer>> graph, boolean [] visited){
+        visited[node] = true;
+
+        for(int next : graph.get(node)){
+            if(next == parent) continue;
+            if(visited[next]) return true;
+            if(hasCycle(next, node, graph, visited)) return true;
+        }
+
+        return false;
+    }
+}
